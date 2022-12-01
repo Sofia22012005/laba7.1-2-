b7961d48098449890683363bc7bf1c0cc66be152
Sofia22012005 <114824939+Sofia22012005@users.noreply.github.com>
2022-12-01 02:22:26 +0200

    Add files via upload

---
+++ laba7.1(2)/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../laba7.1(2)/laba7.1(2).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestMethod1)
+        {
+
+            int count = 0;
+            int k, i, j, S = 0;
+            int result;
+            const int row_count = 4;
+            const int col_count = 4;
+            int a[row_count][col_count] = {
+              {1, 5, 6, 10},
+              {4, 12, 18, 9},
+              {3, -2, 7, 0},
+              {-3, 4, 6, 7}
+            };
+            int* pa[4] = { a[0], a[1], a[2], a[3] };
+            Calc(pa, row_count, col_count, S, k, i, j);
+            result = count;
+
+            Assert::AreEqual(0, result);
+
+        }
+    };
+	
+}
